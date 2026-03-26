12ffe2f88223fd538c80834ebb550e69c9ea14c5
Neal Easterling | Nicolet High School <jimmy.easterling@nicolet.us>
2026-03-26 15:02:51 -0500

    updated logic on spawn

--- animation-frame-refactor/bounce.js
+++ animation-frame-refactor/bounce.js
@@ -1,10 +1,36 @@
 'use strict';
 import {Ball} from './scripts/Ball.js';
+console.log('js connected!');
 
 const bounceContainer = document.getElementById('bounce-container');
+const dropButton = document.getElementById('drop-button');
+const emptyButton = document.getElementById('empty-button');
 
-console.log('js connected!');
-const ball = new Ball().create();
-bounceContainer.appendChild(ball);
-console.log(ball);
+const balls = [];
+let drag = 0.1;
+
+
+
+dropButton.addEventListener('click', ()=>{
+    let ball = new Ball();
+    bounceContainer.appendChild(ball.element);
+    ball.elementRef = document.getElementById(ball.id);
+    balls.push(ball);
+});
+
+emptyButton.addEventListener('click', ()=>{
+    let toRemove = document.getElementsByClassName('ball');
+    while(toRemove.length > 0){
+            toRemove.item(0).remove();
+    }
+    balls.length = 0;
+});
+
+let loop = setInterval(()=>{
+    balls.forEach((ball)=>{
+        ball.y = ball.y + ball.velocity * ball.yDir;
+        ball.elementRef.style.top =  `${ball.y}px`;
+})}, 10);
+
+
 
--- animation-frame-refactor/scripts/Ball.js
+++ animation-frame-refactor/scripts/Ball.js
@@ -2,6 +2,7 @@
 
 export class Ball{
     constructor(){
+        this.id = String(Math.floor(Math.random() * 1000));
         this.x = Math.floor(Math.random() * (450 - 50) + 50);
         this.y = -20;
         this.yDir = 1;
@@ -10,20 +11,18 @@ export class Ball{
         this.color = `rgb(${this.getRandom255()}, ${this.getRandom255()}, ${this.getRandom255()})`;
         this.size = Math.floor(Math.random() * (40 - 10) + 10);
         this.border = this.size / 2;
+        this.element = this.create();
+        this.elementRef = undefined;
     }
     create(){
-        let border = this.size / 2;
         const ball = document.createElement('div');
-        ball.setAttribute('style', `width: ${this.size}px; height: ${this.size}px; border-radius: ${border}px; background-color: ${this.color}`);
-        ball.setAttribute('data-velocity', this.velocity);
-        ball.setAttribute('data-ydir', this.yDir);
-        ball.setAttribute('data-xdir', this.xDir);
+        ball.id = this.id;
+        ball.setAttribute('style', `width: ${this.size}px; height: ${this.size}px; border-radius: ${this.border}px; background-color: ${this.color}`);
         ball.style.top = `${this.y}px`;
         ball.style.left = `${this.x}px`;
         ball.classList.add('ball');
         console.log('ball created');
-        return ball;
-        
+        return ball;  
     }
     getRandomDir(){
         let test  = Math.random();
